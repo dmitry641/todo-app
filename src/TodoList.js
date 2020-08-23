40742bbb939b5651923f9a1d1e4adccb7f4bbe6f
Dmitry <dmitry6416@gmail.com>
2020-08-23 20:00:02 +0300

    use state, props, components

---
+++ src/TodoList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+export default function TodoList({ todos }) {
+  return todos.map((todo) => {
+    return <TodoItem key={todo} todo={todo} />;
+  });
+}
